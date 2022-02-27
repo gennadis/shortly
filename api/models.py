2a7161f2bff9f50ba750cc34f9e2aa0a6c0a2f97
Gennady Badinov <gennady.badinov@gmail.com>
2022-02-27 23:01:20 +0300

    Generate `URL.url_hash` on save

--- api/models.py
+++ api/models.py
@@ -1,4 +1,10 @@
+from random import choice
+from string import ascii_letters, digits
+
 from django.db import models
+
+URL_HASH_SYMBOLS = ascii_letters + digits
+URL_HASH_LENGTH = 7
 
 
 class URL(models.Model):
@@ -14,5 +20,12 @@ class URL(models.Model):
         auto_now_add=True,
     )
 
+    def save(self, *args, **kwargs):
+        if not self.url_hash:
+            self.url_hash = "".join(
+                [choice(URL_HASH_SYMBOLS) for _ in range(URL_HASH_LENGTH)]
+            )
+        super().save(*args, **kwargs)
+
     def __str__(self):
-        return f"Original URL: {self.long_url}, created at: {self.created_at}"
+        return f"Url hash: {self.url_hash}, created at: {self.created_at}"
