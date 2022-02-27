9be881f51fdf9dd39164e3d28d9072edbf91e4c7
Gennady Badinov <gennady.badinov@gmail.com>
2022-02-27 23:30:29 +0300

    Add `URL` viewset

--- api/views.py
+++ api/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+from rest_framework import permissions
+from api.models import URL
+from api.serializers import URLSerializer
+
+
+class URLViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows URLs to be viewed or edited.
+    """
+
+    queryset = URL.objects.all().order_by("-created_at")
+    serializer_class = URLSerializer
+    permission_classes = [permissions.IsAuthenticated]
