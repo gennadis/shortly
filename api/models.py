6828d2da005884a540cc3a2072f5e6ad56d887cd
Gennady Badinov <gennady.badinov@gmail.com>
2022-02-27 22:50:37 +0300

    Create base `URL` model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class URL(models.Model):
+    original_url = models.URLField(verbose_name="Original URL")
+    url_hash = models.CharField(
+        verbose_name="Original URL hash",
+        max_length=20,
+        null=True,
+        blank=True,
+    )
+    created_at = models.DateTimeField(
+        verbose_name="Time URL created at",
+        auto_now_add=True,
+    )
+
+    def __str__(self):
+        return f"Original URL: {self.long_url}, created at: {self.created_at}"
