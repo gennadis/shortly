19e291f7f83e64966b6efee99e2a6c7425677613
Gennady Badinov <gennady.badinov@gmail.com>
2022-02-27 23:36:15 +0300

    Update project urls

--- shortly/urls.py
+++ shortly/urls.py
@@ -15,8 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers
 
+from api import views
+
+router = routers.DefaultRouter()
+router.register(r"/urls", views.URLViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api-auth/", include("rest_framework.urls")),
+    path("v1", include(router.urls)),
+    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
 ]
