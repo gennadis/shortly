6933c46de3ae0db65fb906772d044115b3b636ec
Gennady Badinov <gennady.badinov@gmail.com>
2022-02-28 17:01:41 +0300

    Validate`URL.long_url` reachability

--- api/serializers.py
+++ api/serializers.py
@@ -1,3 +1,4 @@
+import requests
 from rest_framework import serializers
 
 from api.models import URL
@@ -15,3 +16,15 @@ class URLCreationSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = URL
         fields = ["long_url", "short_url"]
+
+    def validate_long_url(self, value):
+        try:
+            response = requests.head(url=value)
+        except requests.RequestException as e:
+            raise serializers.ValidationError("URL is not reachable.")
+        if response.status_code != 200:
+            raise serializers.ValidationError(
+                f"URL is not reachable. Status code: {response.status_code}"
+            )
+
+        return value
