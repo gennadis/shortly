09d6fd3dacc69fde9adad3c7c6707b42e9b8376e
Gennady Badinov <gennady.badinov@gmail.com>
2022-03-01 22:16:16 +0300

    Clean up the `urls`

--- shortly/urls.py
+++ shortly/urls.py
@@ -19,10 +19,26 @@ from django.urls import path, include
 from api import views
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("v1/urls/", views.ListURLView.as_view()),
-    path("v1/urls/<str:hash>/", views.RetrieveUpdateURLView.as_view()),
-    path("v1/shorten/", views.CreateURLView.as_view()),
-    path("<str:hash>/", views.redirect_from_hash),
+    path("admin/", admin.site.urls, name="Admin panel"),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
+    path(
+        "v1/urls/",
+        views.ListURLView.as_view(),
+        name="List all URLs",
+    ),
+    path(
+        "v1/urls/<str:hash>/",
+        views.RetrieveUpdateURLView.as_view(),
+        name="Get or Update an URL",
+    ),
+    path(
+        "v1/shorten/",
+        views.CreateURLView.as_view(),
+        name="Shorten an URL",
+    ),
+    path(
+        "<str:hash>/",
+        views.redirect_from_hash,
+        name="Redirect to original URL",
+    ),
 ]
